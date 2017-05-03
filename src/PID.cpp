3d1d50d05b9419d7afd72024df9bd88388456f6d
Celien Nanson <cesliens@gmail.com>
2017-05-03 19:32:48 +0200

    Finishing initialisation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "PID.h"
 
 using namespace std;
@@ -12,9 +13,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-	this->Kp = Kp; 
-	this->Ki = Ki; 
-	this->Kd = Kd; 
+	this->Kp = Kp; // Proportionnal term
+	this->Ki = Ki; // Integral term 
+	this->Kd = Kd; // Differential term
+
+	// Init the Proportionnal error to a high value
+	// Because, at the very beginning, I assume the car is far from the CTE 
+	this->p_error = numeric_limits<double>::max();  
+
+	// Init the Integral term to 0
+	// Because, at the very beginning, the total area (Integral definition) between the position of the car 
+	// and the CTE is null (because the car has not started yet)
+	this->i_error = 0.0;
+
+	// Init the Differential error to 0
+	// Because, at the very beginning, the position of the car is assumed to be far away from CTE
+	this->d_error = 0.0; 
 }
 
 void PID::UpdateError(double cte) {
